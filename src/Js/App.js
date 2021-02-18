737d93b4dc427be029ab4280305582e54cce0dc8
Anthony N <foto.arn@gmail.com>
2021-02-18 00:28:03 -0800

    + Class component setup

--- src/Js/App.js
+++ src/Js/App.js
@@ -3,7 +3,11 @@ import "../Css/App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>How many of these are we going to do.</h1>
+			<h1>
+				React is a declarative, efficient, and flexible JavaScript library for
+				building user interfaces. It lets you compose complex UIs from small and
+				isolated pieces of code called “components”.
+			</h1>
 		</div>
 	);
 }
