6fac77815435f243f7b3f2f36915fefa4a4df416
Anthony N <foto.arn@gmail.com>
2021-02-17 23:48:27 -0800

    + Getting files all arranged

--- src/Js/App.js
+++ src/Js/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "../Css/App.css";
 
 function App() {
 	return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./Css/index.css";
+import App from "./Js/App";
+import reportWebVitals from "./Js/reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
